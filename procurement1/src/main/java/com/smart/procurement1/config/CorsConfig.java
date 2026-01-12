e18b789137585f87813710dc24eebf60c15f4e11
arulravichandran95 <arulravichandran95@gmail.com>
2026-01-12 10:26:03 +0530

    Update CorsConfig.java

--- procurement1/src/main/java/com/smart/procurement1/config/CorsConfig.java
+++ procurement1/src/main/java/com/smart/procurement1/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://smart-backends.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
