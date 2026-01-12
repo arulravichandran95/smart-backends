1ed50daf38263b6a232e2369870731fd57071b31
arulravichandran95 <arulravichandran95@gmail.com>
2026-01-12 10:39:24 +0530

    Update AuthController.java

--- procurement1/src/main/java/com/smart/procurement1/Controller/AuthController.java
+++ procurement1/src/main/java/com/smart/procurement1/Controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "https://smart-backends.onrender.com")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final UserRepository userRepository;
